a4aac9e256a10d12efdfa8acec80b65b38c6d05c
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-08-19 08:04:26 +0200

    Optimize threshold blocking with numpy

--- thresholdblocking.py
+++ thresholdblocking.py
@@ -1,95 +1,89 @@
 import numpy as np
 
-ns = 1000
-nf = 4
-k = 10
+from scipy.spatial import distance_matrix
+from tqdm import tqdm
+
+ns = 20
+nf = 2
+k = 2
 
 rng = np.random.RandomState(37)
 data = rng.uniform(0, 1, size=(ns, nf))
 
-cost = np.zeros((ns, ns))
-for i in range(ns):
-    for j in range(i + 1, ns):
-        cost_ = np.sqrt(np.sum((data[i,:] - data[j,:])**2))
-        cost[i, j] = cost_
-        cost[j, i] = cost_
+print('Calculate cost matrix')
+cost = distance_matrix(data, data)
 
-# Step 1: Construct (k - 1)-nearest neighbor subgraph of G
+
+# Step 1. Construct (k - 1)-nearest neighbor subgraph.
 vertices = np.array(range(ns))
-edges = []
+edges = np.zeros((ns, ns), dtype=int)
 
-for v in vertices:
+for v in tqdm(vertices, bar_format='Construct (k - 1)-nearest neighbor subgraph {l_bar}{bar:10}{r_bar}{bar:-10b}'):
+    edges[v, v] = 1
     for idx in np.argpartition(cost[v,:], k)[:k]:
-        if idx != v and (idx, v) not in edges:
-            edges.append((v, idx))
+        edges[v, idx] = 1
+        edges[idx, v] = 1
 
-print('V', vertices)
-print('E', edges)
 
 # Step 2.a: Create second power graph
-edges_2 = []
-for e0 in edges:
-    edges_2.append(e0)
-    for e1 in edges:
-        if e0[1] == e1[0] and (e0[0], e1[1]) not in edges_2:
-            edges_2.append((e0[0], e1[1]))
-        if e0[1] == e1[1] and e0[0] != e1[0]:
-            f_0 = e0[0]
-            f_1 = e1[0]
-            if f_0 < f_1:
-                f = (f_0, f_1)
-            else:
-                f = (f_1, f_0)
-            if f not in edges_2:
-                edges_2.append(f)
+edges_2 = edges.copy()
 
+for v0 in tqdm(vertices, bar_format='Calculate second power graph                {l_bar}{bar:10}{r_bar}{bar:-10b}'):
+    for v1 in [v for v in vertices if v > v0]:
+        if edges[v0, v1] == 1:
+            edges_2[v0, :] = np.maximum(edges_2[v0, :], edges[:, v1].T)
+            edges_2[:, v0] = edges_2[v0, :]
 
-print('E^2', edges_2)
-
-# Step 2.b: Create maximal independent set of vertices in second power graph, called block seeds.
-def is_independent(v, seeds, edges):
-    if v in seeds:
-        return False
-
-    for (e0, e1) in edges:
-        if e0 == v and e1 in seeds:
-            return False
-        if e1 == v and e0 in seeds:
-            return False
-
-    return True
-
+# Step 2.b: Create maximal independent set of vertices in second power graph,
+#   called block seeds.
 seeds = []
-for v in vertices:
-    if is_independent(v, seeds, edges_2):
+for v in tqdm(vertices, bar_format='Collect seeds                               {l_bar}{bar:10}{r_bar}{bar:-10b}'):
+    if not np.any(edges_2[v, np.array(seeds, dtype=int)]):
         seeds.append(v)
 
-print('Seeds', seeds)
-
 # Step 3: For each seed, create a block comprising its closed neighborhood.
-unassigned = list(vertices.copy())
 blocks = []
-for s in seeds:
-    block = [s]
-    unassigned.remove(s)
-    for e in edges:
-        if e[0] == s and e[1] in unassigned:
-            block.append(e[1])
-            unassigned.remove(e[1])
-        elif e[1] == s and e[0] in unassigned:
-            block.append(e[0])
-            unassigned.remove(e[0])
+unassigned = list(vertices)
+for s in tqdm(seeds, bar_format='Create threshold blocks                     {l_bar}{bar:10}{r_bar}{bar:-10b}'):
+    block = []
+    for v in vertices:
+        if v in unassigned:
+            if edges[s, v] == 1:
+                block.append(v)
+                unassigned.remove(v)
+
     blocks.append(block)
 
 
-# Step 4: Add unassigned vertices to blocks.
-print('Unassigned', unassigned)
+# Step 4: Add unassign vertices to block with smallest distance.
+print('Add all remaining vertices to threshold blocks')
+
+def connected(v, block):
+    for b in block:
+        if edges[v, b] == 1:
+            return True
+    return False
+
+def distance(v, block):
+    return np.mean(cost[v, block])
+
+for v in unassigned:
+    i = np.argmin([distance(v, block) for block in blocks])
+    blocks[i].append(v)
+unassigned = []
+
+def within_distance(block):
+    s = 0
+    for b0 in block:
+        for b1 in block:
+            s += cost[b0, b1]
+    return s
 
 for block in blocks:
-    for vb in block:
-        for vu in unassigned:
-            if (vu, vb) in edges or (vb, vu) in edges:
-                block.append(vu)
-                unassigned.remove(vu)
-
-print('Blocks', blocks)
+    print(
+        block,
+        'size',
+        len(block),
+        round(within_distance(block), 2),
+        round(within_distance(rng.choice(vertices, len(block), replace=False)), 2)
+    )
